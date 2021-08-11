132c8ca4fdca75933c54fb9894a82da5cb06482a
Will <fesaba21@gmail.com>
2021-08-11 15:21:52 -0500

    Proyecto inicializado

---
+++ cmake-build-debug/CMakeFiles/Taller_Colaborativo_Grupo_E.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Taller_Colaborativo_Grupo_E.dir/main.cpp.o"
+  "Taller_Colaborativo_Grupo_E"
+  "Taller_Colaborativo_Grupo_E.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Taller_Colaborativo_Grupo_E.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Taller_Colaborativo_Grupo_E.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Taller_Colaborativo_Grupo_E.
+# This may be replaced when dependencies are built.
